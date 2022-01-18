ae9cfee927b2010f6b5504ab95bfaa259762794e
weiss <1554783799@qq.com>
2022-01-18 15:36:02 +0800

    初始化

---
+++ src/Demo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace weiss;
+
+class Demo
+{
+
+    public function index1() : int
+    {
+        return 123;
+    }
+
+    public function index2() : int
+    {
+        return 456;
+    }
+}
---
+++ src/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+require "../vendor/autoload.php";
+
+var_dump(123);
+
+$demo = new \weiss\Demo();
+
+var_dump(333);
+
+var_dump($demo->index1());
+var_dump($demo->index2());
+
